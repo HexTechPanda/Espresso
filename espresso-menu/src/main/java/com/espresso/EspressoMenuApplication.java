4bacd8efb01d3ebc958fbcc8ca5292303c563640
HexTechPanda <875038802@qq.com>
2022-10-07 16:20:11 +0800

    Initial commit

---
+++ espresso-menu/src/main/java/com/espresso/EspressoMenuApplication.java
@@ -0,0 +1,13 @@
+package com.espresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspressoMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspressoMenuApplication.class, args);
+    }
+
+}
---
+++ espresso-menu/src/test/java/com/espresso/EspressoMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspressoMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
