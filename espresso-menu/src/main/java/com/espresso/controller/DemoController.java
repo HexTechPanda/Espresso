a01dae7d9a28734bcda6db0ba00d81135bfa7974
HexTechPanda <875038802@qq.com>
2022-10-09 20:15:41 +0800

    Add health check

--- espresso-menu/src/main/java/com/espresso/controller/DemoController.java
+++ espresso-menu/src/main/java/com/espresso/controller/DemoController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    @GetMapping("/")
+    public String healthCheck() {
+        return "HEALTH CHECK OK!";
+    }
+
     @GetMapping("/hello")
     public String helloWorld(){
         return "Hello world!";
