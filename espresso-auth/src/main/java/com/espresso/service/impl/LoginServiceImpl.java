51b264b375e5a6249a00bd28269e7952f07fb57e
HexTechPanda <875038802@qq.com>
2022-11-02 14:58:30 +0800

    Fix login fail response.

--- espresso-auth/src/main/java/com/espresso/service/impl/LoginServiceImpl.java
+++ espresso-auth/src/main/java/com/espresso/service/impl/LoginServiceImpl.java
@@ -3,6 +3,7 @@ package com.espresso.service.impl;
 
 import com.espresso.commons.model.UserProfile;
 import com.espresso.commons.result.Result;
+import com.espresso.commons.result.ResultEnum;
 import com.espresso.domain.LoginRequest;
 import com.espresso.domain.LoginUser;
 import com.espresso.dto.SysUser;
@@ -37,41 +38,45 @@ public class LoginServiceImpl implements LoginService {
         // 3 Authenticate using the ProviderManager auth method
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                 new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword());
-        Authentication authenticate = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        try{
+            Authentication authenticate = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
 
-        // Authenticate fail
-        if(Objects.isNull(authenticate)){
-            throw new BadCredentialsException("username or password error");
+            // Authenticate fail
+            if(Objects.isNull(authenticate)){
+                throw new BadCredentialsException("username or password error");
+            }
+
+            // 4 Generate jwt to the front end
+            LoginUser loginUser= (LoginUser) (authenticate.getPrincipal());
+            SysUser sysUser = loginUser.getSysUser();
+            String userId = sysUser.getId();
+            String jwt = JwtUtil.createJWT(userId);
+            Map<String, String> map = new HashMap();
+            map.put("token",jwt);
+            // 5 Put all information related to system users into Redis
+            UserProfile userProfile = new UserProfile();
+            userProfile.setId(userId);
+            userProfile.setUsername(sysUser.getUsername());
+            userProfile.setNickName(sysUser.getNickName());
+            userProfile.setMobile(sysUser.getMobile());
+            userProfile.setEmail(sysUser.getEmail());
+            userProfile.setPermissions(loginUser.getPermissions());
+
+            redisCache.setCacheObject("login:" + userId, userProfile, 1800, TimeUnit.SECONDS);
+            return Result.ok("Login success.", map);
+        } catch (BadCredentialsException exception){
+            return Result.error("Username or password error.");
+        } catch (Exception exception){
+            return Result.build(ResultEnum.AUTH_FAIL);
         }
-
-        // 4 Generate jwt to the front end
-        LoginUser loginUser= (LoginUser) (authenticate.getPrincipal());
-        SysUser sysUser = loginUser.getSysUser();
-        String userId = sysUser.getId();
-        String jwt = JwtUtil.createJWT(userId);
-        Map<String, String> map = new HashMap();
-        map.put("token",jwt);
-        // 5 Put all information related to system users into Redis
-        UserProfile userProfile = new UserProfile();
-        userProfile.setId(userId);
-        userProfile.setUsername(sysUser.getUsername());
-        userProfile.setNickName(sysUser.getNickName());
-        userProfile.setMobile(sysUser.getMobile());
-        userProfile.setEmail(sysUser.getEmail());
-        userProfile.setPermissions(loginUser.getPermissions());
-
-        redisCache.setCacheObject("login:" + userId, userProfile, 1800, TimeUnit.SECONDS);
-
-        return Result.ok("Login success.", map);
     }
 
     @Override
     public Result logout() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        LoginUser loginUser = (LoginUser) authentication.getPrincipal();
-        String userId = loginUser.getSysUser().getId();
+        UserProfile userProfile = (UserProfile) authentication.getPrincipal();
+        String userId = userProfile.getId();
         redisCache.deleteObject("login:" + userId);
-
         return Result.ok();
     }
 
