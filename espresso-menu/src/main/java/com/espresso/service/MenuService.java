a2c213940e25528c8f870ad09f757400cf5a90f3
Mingze Ma <mingzema.1998@foxmail.com>
2022-11-04 16:28:07 +0800

    feature: menu service init

---
+++ espresso-menu/src/main/java/com/espresso/service/MenuService.java
@@ -0,0 +1,13 @@
+package com.espresso.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Menu Service Interface
+ *
+ * @author Mingze Ma
+ */
+@Service
+public interface MenuService {
+
+}
---
+++ espresso-menu/src/main/java/com/espresso/service/impl/MenuServiceImpl.java
@@ -0,0 +1,13 @@
+package com.espresso.service.impl;
+
+import com.espresso.service.MenuService;
+
+/**
+ * Menu Service Impl
+ *
+ * @author Mingze Ma
+ */
+public class MenuServiceImpl implements MenuService {
+
+
+}
